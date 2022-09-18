8d9e6cfee95569d686bfbf0e108a0ef32674e508
chase-compton <cscompton1@crimson.ua.edu>
2022-09-18 00:53:15 -0500

    third

--- CircularDynamicArray.cpp
+++ CircularDynamicArray.cpp
@@ -29,7 +29,7 @@ public:
         front = 0;
     }
 
-    CircularDynamicArray(const CircularDynamicArray& rhs)
+    CircularDynamicArray(const CircularDynamicArray &rhs)
     {
         size = rhs.size;
         cap = rhs.cap;
@@ -56,7 +56,7 @@ public:
             newArray = new type[cap];
             for (int i = 0; i < size; i++)
             {
-                newArray[i] = array[i];
+                newArray[i] = *(array + ((front + i) % cap));
             }
             for (int j = size; j < cap; j++)
             {
@@ -76,12 +76,13 @@ public:
         {
             type *newArray;
             cap = cap * 2;
+            front = (front - 1 + cap) % cap;
+            array[front] = value;
             size++;
             newArray = new type[cap];
-            newArray[0] = value;
-            for (int i = 1; i < size; i++)
+            for (int i = 0; i < size; i++)
             {
-                newArray[i] = array[i - 1];
+                newArray[i] = *(array + ((front + i) % cap));
             }
             for (int j = size; j < cap; j++)
             {
@@ -109,7 +110,7 @@ public:
             newArray = new type[cap];
             for (int i = 0; i < size; i++)
             {
-                newArray[i] = array[i];
+                newArray[i] = *(array + ((front + i) % cap));
             }
             for (int j = size; j < cap; j++)
             {
@@ -132,7 +133,7 @@ public:
             newArray = new type[cap];
             for (int i = 0; i < size; i++)
             {
-                newArray[i] = array[i];
+                newArray[i] = *(array + ((front + i) % cap));
             }
             for (int j = size; j < cap; j++)
             {
@@ -162,17 +163,85 @@ public:
 
     type QuickSelect(int k)
     {
-
     }
 
     type WCSelect(int k)
     {
-
     }
 
     void stableSort()
     {
+        type *tempArray = new type[size];
+        for(int i = 0; i < size; i++)
+        {
+            tempArray[i] = *(array + ((front + i) % cap));
+        }
+        mergeSort(tempArray, 0, size - 1);
+        delete[] array;
+        array = tempArray;
+        front = 0;
+    }
 
+    void mergeSort(type tempArray[], int begin, int end)
+    {
+        if (begin >= end)
+        {
+            return;
+        }
+        int mid = begin + (end - begin) / 2;
+        mergeSort(tempArray, begin, mid);
+        mergeSort(tempArray, mid + 1, end);
+        merge(tempArray, begin, mid, end);
+    }
+
+    void merge(type tempArray[], int left, int mid, int right)
+    {
+        int subOne = mid - left + 1;
+        int subTwo = right - mid;
+
+        type *leftArray = new type[subOne];
+        type *rightArray = new type[subTwo];
+
+        for (int i = 0; i < subOne; i++)
+        {
+            leftArray[i] = tempArray[left + i];
+        }
+        for (int j = 0; j < subTwo; j++)
+        {
+            rightArray[j] = tempArray[mid + 1 + j];
+        }
+
+        int indexOne = 0, indexTwo = 0;
+        int indexMerged = left;
+
+        while (indexOne < subOne && indexTwo < subTwo)
+        {
+            if (leftArray[indexOne] <= rightArray[indexTwo])
+            {
+                tempArray[indexMerged] = leftArray[indexOne];
+                indexOne++;
+            }
+            else
+            {
+                tempArray[indexMerged] = rightArray[indexTwo];
+                indexTwo++;
+            }
+            indexMerged++;
+        }
+        while (indexOne < subOne)
+        {
+            tempArray[indexMerged] = leftArray[indexOne];
+            indexOne++;
+            indexMerged++;
+        }
+        while (indexTwo < subTwo)
+        {
+            tempArray[indexMerged] = rightArray[indexTwo];
+            indexTwo++;
+            indexMerged++;
+        }
+        delete [] leftArray;
+        delete [] rightArray;
     }
 
     int linearSearch(type e)
@@ -181,7 +250,7 @@ public:
         {
             if (*(array + ((front + i) % cap)) == e)
             {
-                return  i;
+                return i;
             }
         }
         return -1;
@@ -189,6 +258,26 @@ public:
 
     int binSearch(type e)
     {
+        int left = front;
+        int right = (front + size - 1) % cap;
+        int mid;
+
+        while (left <= right)
+        {
+            mid = left + (right - left) / 2;
+            if (array[mid] == e)
+            {
+                return mid;
+            }
+            else if (array[mid] > e)
+            {
+                right = mid - 1;
+            }
+            else
+            {
+                left = mid + 1;
+            }
+        }
         return -1;
     }
 };
--- test.cpp
+++ test.cpp
@@ -4,38 +4,46 @@ using namespace std;
 
 
 int main(){
-	CircularDynamicArray<float> C(10);
-	for (int i=0; i< C.length();i++) C[i] = i;
-	for (int i=0; i< C.length();i++) cout << C[i] << " ";  cout << endl;
-	// C => "0 1 2 3 4 5 6 7 8 9"
-    cout<<"----"<<endl;
-	C.addEnd(10);
-	for (int i=0; i< C.length();i++) cout << C[i] << " ";  cout << endl;
-    cout<<"----"<<endl;
-    C.addFront(11);
-    for (int i=0; i< C.length();i++) cout << C[i] << " ";  cout << endl;
-    cout<<"----"<<endl;
-    C.addFront(12);
-    for (int i=0; i< C.length();i++) cout << C[i] << " ";  cout << endl;
-    cout<<"----"<<endl;
-    C.addEnd(13);
-    for (int i=0; i< C.length();i++) cout << C[i] << " ";  cout << endl;
-    cout<<"----"<<endl;
-    C.delEnd();
-    for (int i=0; i< C.length();i++) cout << C[i] << " ";  cout << endl;
-    cout<<"----"<<endl;
-    C.delFront();
-    for (int i=0; i< C.length();i++) cout << C[i] << " ";  cout << endl;
-    cout<<"----"<<endl;
-    C.delFront();
-    for (int i=0; i< C.length();i++) cout << C[i] << " ";  cout << endl;
-    cout<<"----"<<endl;
-    C.delEnd();
-    for (int i=0; i< C.length();i++) cout << C[i] << " ";  cout << endl;
-    cout<<"----"<<endl;
-    C.addFront(11);
-    for (int i=0; i< C.length();i++) cout << C[i] << " ";  cout << endl;
-    cout<<"----"<<endl;
+	CircularDynamicArray<int> A,B;
+	for(int i=0; i<10;i++) A.addEnd(i);
+	B = A;
+    cout<<"A:"<<endl;
+    for (int i=0; i< A.length();i++) cout << A[i] << " ";  cout << endl;
+    cout<<"B:"<<endl;
+    for (int i=0; i< B.length();i++) cout << B[i] << " ";  cout << endl;
+	A.addEnd(15); A.addEnd(19);
+    cout<<"A:"<<endl;
+    for (int i=0; i< A.length();i++) cout << A[i] << " ";  cout << endl;
+	// A => "0 1 2 3 4 5 6 7 8 9 15 19" 
+	cout << "Select is " << A.linearSearch(5) << endl;
+	// A => "0 1 2 3 4 5 6 7 8 9 15 19" Search => 5
+	cout << "Select is " << A.binSearch(12) << endl;
+	// A => "0 1 2 3 4 5 6 7 8 9 15 19" Search => -1
+	cout << "Select is " << A.binSearch(15) << endl;
+	// A => "0 1 2 3 4 5 6 7 8 9 15 19" Search => 10	
+	A.addFront(10);
+    cout<<"A:"<<endl;
+    for (int i=0; i< A.length();i++) cout << A[i] << " ";  cout << endl; 
+	// A => "10 0 1 2 3 4 5 6 7 8 9 15 19"
+	cout << "Select is " << A.linearSearch(5) << endl;
+	// A => "10 0 1 2 3 4 5 6 7 8 9 15 19" Search => 6
+	A.stableSort();
+    cout<<"A:"<<endl;
+    for (int i=0; i< A.length();i++) cout << A[i] << " ";  cout << endl;
+	// A => "0 1 2 3 4 5 6 7 8 9 10 15 19"
+	A.addEnd(11); A.addFront(1); A.addFront(2);
+	cout << "capacity is " << A.capacity() << endl;
+    cout<<"A:"<<endl;
+    for (int i=0; i< A.length();i++) cout << A[i] << " ";  cout << endl;
+	// A => "3 2 1 0 1 2 3 4 5 6 7 8 9 10 15 19 11"	  capacity => 32
+    A.addFront(3);
+    cout << "capacity is " << A.capacity() << endl;
+    cout<<"A:"<<endl;
+    for (int i=0; i< A.length();i++) cout << A[i] << " ";  cout << endl;
+	A.delFront(); A.delFront();
+    cout<<"A:"<<endl;
+    for (int i=0; i< A.length();i++) cout << A[i] << " ";  cout << endl;
+	// A => "1 0 1 2 3 4 5 6 7 8 9 10 15 19 11"	  capacity => 32
     
     
 
